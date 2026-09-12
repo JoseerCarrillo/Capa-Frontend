0bc011abd3de4648e815e5569ebef4db4a432956
USUARIO <joseersneidercg@ufps.edu.co>
2026-09-12 16:24:35 -0500

    cambios del js hechos para el dashboard

--- scripts.js
+++ scripts.js
@@ -2,21 +2,20 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     var loginForm = document.getElementById("loginForm");
-    loginForm.addEventListener("submit", function(event){
-        event.preventDefault();
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    if(loginForm){
+        loginForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
 
-        if(username === "admin" && password === "1234"){
-            window.location.href = "dashboard.html";
-        } else{
-            alert("Usuario o contraseña incorrectos");
-        }
+            if(username === "admin" && password === "1234"){
+                window.location.href = "dashboard.html";
+            } else{
+                alert("Usuario o contraseña incorrectos");
+            }
+        });
+    }
 
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function(){
     var registroForm = document.getElementById("registroForm");
     if(registroForm){
         registroForm.addEventListener("submit", function(event){
@@ -28,4 +27,21 @@ document.addEventListener("DOMContentLoaded", function(){
             window.location.href = "index.html";
         });
     }
+var linkEstudiantes = document.getElementById("linkEstudiantes");
+var linkAsignaturas = document.getElementById("linkAsignaturas");
+
+var contenedorEstudiantes = document.getElementById("contenedorEstudiantes");
+var contenedorAsignaturas = document.getElementById("contenedorAsignaturas");
+
+linkEstudiantes.addEventListener("click", function(event){
+    event.preventDefault();
+    contenedorEstudiantes.classList.add("active");
+    contenedorAsignaturas.classList.remove("active");
+});
+
+linkAsignaturas.addEventListener("click", function(event){
+    event.preventDefault();
+    contenedorAsignaturas.classList.add("active");
+    contenedorEstudiantes.classList.remove("active");
+});
 });
